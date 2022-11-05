0d3654a07d08de790df1dbc83caabd24b69e0199
JaJa <JaJa@ares.com.tw>
2022-11-05 22:43:29 +0800

    Changes： * jsql.TableSchema PrimaryKey column type from string to interface{}

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# v1.2.14 (2022.11.05)
+Changes：
+* jsql.TableSchema PrimaryKey column type from string to interface{}
+
 # v1.2.13 (2022.11.05)
 Changes：
 * jsql.TableSchema PrimaryKey column type from int8 to string
--- jsql/table_schema.go
+++ jsql/table_schema.go
@@ -11,7 +11,7 @@ type TableSchema struct {
 	// DataDefault column default data
 	DataDefault string `json:"DATA_DEFAULT"`
 	// PrimaryKey primary key
-	PrimaryKey string `json:"PRIMARY_KEY"`
+	PrimaryKey interface{} `json:"PRIMARY_KEY"`
 	// IsIdentity is identity(YES OR NO)
 	IsIdentity string `json:"IS_IDENTITY"`
 	// ColumnComment column comment
